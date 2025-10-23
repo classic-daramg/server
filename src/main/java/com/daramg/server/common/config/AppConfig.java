cbe0564439a0fc2e58e17f4de7c4063155e33743
양효빈 <hyobin3026@naver.com>
2025-10-23 19:28:09 +0900

    feat: AppConfig 설정

---
+++ src/main/java/com/daramg/server/common/config/AppConfig.java
@@ -0,0 +1,29 @@
+package com.daramg.server.common.config;
+
+import com.daramg.server.auth.resolver.AuthUserResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class AppConfig implements WebMvcConfigurer {
+
+    private final AuthUserResolver authUserResolver;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authUserResolver);
+    }
+}
