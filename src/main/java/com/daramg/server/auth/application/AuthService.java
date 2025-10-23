08ac453d558bcf5e2ee523636cf4d4ce48f6f41f
양효빈 <hyobin3026@naver.com>
2025-10-23 19:27:57 +0900

    feat: 로그인, 로그아웃 api 구현

--- src/main/java/com/daramg/server/auth/application/AuthService.java
+++ src/main/java/com/daramg/server/auth/application/AuthService.java
@@ -1,24 +1,39 @@
 package com.daramg.server.auth.application;
 
+import com.daramg.server.auth.dto.TokenResponseDto;
+import com.daramg.server.auth.exception.AuthErrorStatus;
+import com.daramg.server.auth.util.JwtUtil;
 import com.daramg.server.common.exception.BusinessException;
 import com.daramg.server.domain.user.domain.User;
 import com.daramg.server.auth.domain.SignupVo;
-import com.daramg.server.auth.dto.LoginDto;
-import com.daramg.server.auth.dto.PasswordDto;
-import com.daramg.server.auth.dto.SignupDto;
+import com.daramg.server.auth.dto.LoginRequestDto;
+import com.daramg.server.auth.dto.PasswordRequestDto;
+import com.daramg.server.auth.dto.SignupRequestDto;
 import com.daramg.server.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class AuthService {
 
-    private final UserRepository userRepository;
+    @Value("${jwt.refresh-time}")
+    private long REFRESH_TOKEN_VALID_TIME;
 
-    public void signup(SignupDto dto){
+    private final UserRepository userRepository;
+    private final JwtUtil jwtTokenProvider;
+    private final PasswordEncoder passwordEncoder;
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public void signup(SignupRequestDto dto){
         if (userRepository.existsByEmail(dto.getEmail())) {
             throw new BusinessException("중복된 이메일입니다.");
         }
@@ -39,10 +54,54 @@ public class AuthService {
         userRepository.save(user);
     }
 
-    public void login(LoginDto dto){
+    public TokenResponseDto login(LoginRequestDto dto){
+        User user = userRepository.findByEmail(dto.getEmail())
+                .filter(u -> passwordEncoder.matches(dto.getPassword(), u.getPassword()))
+                .orElseThrow(() -> new BusinessException(AuthErrorStatus.USER_NOT_FOUND_EXCEPTION));
+
+        TokenResponseDto tokens = jwtTokenProvider.generateTokens(user);
+        redisTemplate.opsForValue().set(
+                user.getEmail(),
+                tokens.getRefreshToken(),
+                REFRESH_TOKEN_VALID_TIME,
+                TimeUnit.MILLISECONDS
+        );
+        return tokens;
     }
 
-    public void resetPassword(PasswordDto dto, User user){
+    public TokenResponseDto refreshAccessToken(String refreshToken) {
+        if (!jwtTokenProvider.validateRefreshToken(refreshToken)) {
+            throw new BusinessException(AuthErrorStatus.INVALID_TOKEN_EXCEPTION);
+        }
+
+        String userEmail = jwtTokenProvider.getUserEmail(refreshToken);
+        String originRefreshToken = redisTemplate.opsForValue().get(userEmail);
+
+        if (originRefreshToken == null || !originRefreshToken.equals(refreshToken)) {
+            throw new BusinessException(AuthErrorStatus.INVALID_TOKEN_EXCEPTION);
+        }
+
+        User user = userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new BusinessException(AuthErrorStatus.USER_NOT_FOUND_EXCEPTION));
+
+        String newAccessToken = jwtTokenProvider.createAccessToken(user);
+        return new TokenResponseDto(newAccessToken, refreshToken);
+    }
+
+    public void logout(User user){
+        redisTemplate.delete(user.getEmail());
+    }
+
+    public void resetPassword(PasswordRequestDto dto){
+        User user = userRepository.findByEmail(dto.getEmail())
+                .orElseThrow(() -> new BusinessException(AuthErrorStatus.USER_NOT_FOUND_EXCEPTION));
+
         user.changePassword(dto.getPassword());
+        logout(user);
+    }
+
+    public User loadUserByEmail(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new BusinessException("(email: " + email + ")", AuthErrorStatus.USER_NOT_FOUND_EXCEPTION));
     }
 }
--- src/main/java/com/daramg/server/auth/dto/PasswordRequestDto.kt
+++ src/main/java/com/daramg/server/auth/dto/PasswordRequestDto.kt
@@ -1,8 +1,13 @@
 package com.daramg.server.auth.dto
 
+import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.Pattern
 
-class PasswordDto(
+data class PasswordRequestDto(
+
+    @field:Email(message = "AUTH_400_3")
+    val email: String,
+
     @get:Pattern(
         regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%^&*()]).{10,}\$",
         message = "비밀번호는 영어 대/소문자, 숫자, 특수문자를 모두 포함하여 10자 이상이어야 합니다"
