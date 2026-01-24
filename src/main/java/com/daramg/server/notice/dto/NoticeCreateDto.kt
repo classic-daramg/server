37c3dcafeeed37b67bd77a83714b62252a5782e5
Minjae Chung <kevinc02@naver.com>
2026-01-25 03:58:48 +0900

    [feat] notice DTO 추가

---
+++ src/main/java/com/daramg/server/notice/dto/NoticeCreateDto.kt
@@ -0,0 +1,29 @@
+package com.daramg.server.notice.dto
+
+import com.daramg.server.notice.domain.Notice
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+
+class NoticeCreateDto (
+
+    @get:NotBlank(message = "제목 입력은 필수입니다")
+    @get:Size(max = 15, message = "15자 이내로 입력해주세요")
+    val title: String,
+
+    @get:NotBlank(message = "내용 입력은 필수입니다")
+    @get:Size(min = 5, max = 3000, message ="5자 이상 3000자 이내로 입력해주세요")
+    val content: String,
+
+    val image: List<String> = emptyList(),
+
+    val videoUrl: String? = null,
+
+) {
+    fun toEntity() : Notice =
+        Notice(
+            title,
+            content,
+            image,
+            videoUrl
+        )
+}
