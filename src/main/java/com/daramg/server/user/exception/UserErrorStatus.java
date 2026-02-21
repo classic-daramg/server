4a07aa845a48f6db3b7618d24fb2d84204dd99db
Minjae Chung <kevinc02@naver.com>
2026-02-21 22:24:12 +0900

    [fix] NOT_FOLLOWING 에러 메시지 테스트 기대값과 일치하도록 수정

--- src/main/java/com/daramg/server/user/exception/UserErrorStatus.java
+++ src/main/java/com/daramg/server/user/exception/UserErrorStatus.java
@@ -17,7 +17,7 @@ public enum UserErrorStatus implements BaseErrorCode {
     SELF_FOLLOW(HttpStatus.BAD_REQUEST, ErrorCategory.USER.generate(400_2), "팔로우 대상과 주체의 유저가 동일합니다."),
     ALREADY_FOLLOWING(HttpStatus.CONFLICT, ErrorCategory.USER.generate(409_2), "이미 팔로우하고 있는 상태입니다."),
     SELF_UNFOLLOW(HttpStatus.BAD_REQUEST, ErrorCategory.USER.generate(400_3), "언팔로우 대상과 주체의 유저가 동일합니다."),
-    NOT_FOLLOWING(HttpStatus.BAD_REQUEST, ErrorCategory.USER.generate(400_4), "팔로우하지 않은 유저입니다.");
+    NOT_FOLLOWING(HttpStatus.BAD_REQUEST, ErrorCategory.USER.generate(400_4), "언팔로우할 유저를 팔로우하지 않은 상태입니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
